519df9c30d9c107b2f69e00caf92d97ab683161e
HONGMINJU <mhong0800@gmail.com>
2022-05-26 01:56:23 +0900

    :sparkles: : GlobalExceptionAdvisor 생성

---
+++ src/main/java/com/bookflix/bookflix/common/advisor/GlobalExceptionAdvisor.java
@@ -0,0 +1,9 @@
+package com.bookflix.bookflix.common.advisor;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvisor {
+
+
+}
