28be415ffdb4d1d9cc96c967d9fca8e87d7f4e3e
HONGMINJU <mhong0800@gmail.com>
2022-06-03 00:10:27 +0900

    :sparkles: : BaseResponseStatus에 oauth, user관련 에러 추가

--- src/main/java/com/bookflix/bookflix/common/response/BaseResponseStatus.java
+++ src/main/java/com/bookflix/bookflix/common/response/BaseResponseStatus.java
@@ -12,8 +12,19 @@ public enum BaseResponseStatus {
     SUCCESS(BaseResponseResultStatus.SUCCESS, 1000, "요청에 성공하였습니다."),
 
     /**
-     * 2000 : 회원 관련 실패
+     * 2000 : OAUTH 관련 실패
      */
+    INVALID_JWT(FAILED, 2000, "유효하지 않은 JWT입니다."),
+    EMPTY_JWT(FAILED, 2001, "빈 JWT입니다."),
+    EXPIRED_JWT(FAILED, 2002, "만료된 JWT입니다."),
+    POST_USERS_KAKAO_ERROR(FAILED, 2003, "카카오에서 정보를 가져오는 과정에서 에러가 발생했습니다."),
+    POST_USERS_NAVER_ERROR(FAILED, 2004, "네이버에서 정보를 가져오는 과정에서 에러가 발생했습니다."),
+
+    /**
+     * 3000 : 사용자 관련 실패
+     */
+    USER_NOT_EXIST(FAILED, 3000, "존재하지 않는 user입니다."),
+    INVALID_USERID(FAILED, 3001, "유효하지 않은 userId입니다."),
     ;
 
     private final BaseResponseResultStatus resultStatus;
